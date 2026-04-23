3a8a7f6e995aeea420e797dd568020caca697c64
wddxg <48612121+wddxg@users.noreply.github.com>
2026-04-23 14:20:16 +0800

    Add scripts/local_embedding_service.py

---
+++ scripts/local_embedding_service.py
@@ -0,0 +1,11 @@
+from zotero_mcp_augmentation.embedding import (
+    EmbeddingRuntimeFactory,
+    LocalEmbeddingRuntime,
+    SentenceTransformerRuntime,
+)
+
+__all__ = [
+    "EmbeddingRuntimeFactory",
+    "LocalEmbeddingRuntime",
+    "SentenceTransformerRuntime",
+]
