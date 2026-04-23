0b84ccb971baf27c27363f5f8bea10f734a7766c
wddxg <48612121+wddxg@users.noreply.github.com>
2026-04-23 14:20:13 +0800

    Add scripts/chunk_models.py

---
+++ scripts/chunk_models.py
@@ -0,0 +1,19 @@
+from zotero_mcp_augmentation.models import (
+    ChunkDocument,
+    ChunkRecord,
+    ChunkingConfig,
+    HeadingInfo,
+    HeadingState,
+    QueryConfig,
+    StoreBuildConfig,
+)
+
+__all__ = [
+    "ChunkDocument",
+    "ChunkRecord",
+    "ChunkingConfig",
+    "HeadingInfo",
+    "HeadingState",
+    "QueryConfig",
+    "StoreBuildConfig",
+]
